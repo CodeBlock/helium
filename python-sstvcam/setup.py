99e05467714485ddf2a205d97e45074d91078fac
Ricky Elrod <ricky@elrod.me>
2013-07-27 21:05:42 -0400

    Make python-sstv into a python module.

---
+++ python-sstvcam/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='sstvcam',
+      version='0',
+      description='A library for preparing images (taken from the RPi camera) to send via SSTV',
+      author='Ricky Elrod',
+      author_email='ricky@elrod.me',
+      url='https://github.com/CodeBlock/helium',
+      packages=['sstvcam'],
+)
